390e60dde656381f796e74d2e5fb2d96f208778f
dkoprowski <dkoprowski@outlook.com>
2016-01-17 21:28:50 +0100

    init

---
+++ Recognition-of-Handwritten-Digits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recognition_of_Handwritten_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
